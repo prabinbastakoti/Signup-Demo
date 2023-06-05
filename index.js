b0ee37fe0d2115c0ec47aaf90296cf6db2c45ef1
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-06-06 02:12:38 +0545

    Rework Password Match Validation

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 
 const password = document.querySelector('input[name="password"]');
 const confirmPassword = document.querySelector('input[name="confirmpassword"]');
-const error = document.querySelector('.error');
+const error = document.querySelectorAll('.error');
 
 
 confirmPassword.addEventListener('keyup', (event) => {
     if (password.value != event.target.value) {
-        error.textContent = "*Password did not match";
+        error.forEach(item => {
+            item.textContent = "*Password did not match";
+            password.style.border = '1px solid red';
+            confirmPassword.style.border = '1px solid red';
+        });
     }
     else {
-        error.innerHTML = '&nbsp;';
+        error.forEach(item => {
+            item.textContent = "";
+            password.style.border = '1px solid blue';
+            confirmPassword.style.border = '1px solid blue';
+        });
     }
 })
